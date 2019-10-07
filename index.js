b645bfa1d196441091986f09e0cc926448207cbb
Sawqii <47114999+Sawqii@users.noreply.github.com>
2019-10-07 12:54:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-bot.login(process.env.BOT_TOKEN)
+bot.login("NjI1MzY4OTg0NTM0OTA4OTI4.XZsUOA.KsHLXQx4fH5cfJhU7TMQbW1OnFc");
 
 bot.on("guildMemberAdd", user =>{
     user.guild.channels.get("630116206216478740").send("Bienvenue " + user + " sur le serveur " + user.guild.name + " ! :yum: :man_dancing:" )
