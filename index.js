b429e42f519e58a697ae31c48589f566f2af9bf9
Sawqii <47114999+Sawqii@users.noreply.github.com>
2019-10-07 12:47:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.login(process.env.BOT_TOKEN)
+
+bot.on("guildMemberAdd", user =>{
+    user.guild.channels.get("630116206216478740").send("Bienvenue " + user + " sur le serveur " + user.guild.name + " ! :yum: :man_dancing:" )
+});
+
+bot.on('message', msg => {
+    if (msg.content === prefix + 'help') {
+      msg.reply('pong');
+    }
+  });
